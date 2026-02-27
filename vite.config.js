46cae0e6eead2eebc2ba6084caf80639ad54a222
NateGMackie <ng.mackie@gmail.com>
2026-02-27 14:47:08 -0500

    vite fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Word-to-HTML-WYSIWYG/',
+  base: '/TypeSet/',
   plugins: [react()],
   optimizeDeps: {
     entries: ['index.html'],
